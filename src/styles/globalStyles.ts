b69d42bb0e93c4f76d4597c4182811a80e0e488b
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-16 13:45:08 +0900

    style: ID root 스타일 추가

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -8,5 +8,11 @@ export const GlobalStyle = createGlobalStyle`
  }
  body {
   color:${({ theme }) => theme.fontColor};
+  font-family: 'Roboto', sans-serif;
+  #root {
+    width: 390px;
+   margin:0 auto;
+   background-color: ${({ theme }) => theme.bgColor};
+  }
  }
  `;
