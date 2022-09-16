7ac3f449b8aeab0c2c70c272244a0fc798a40c1a
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-16 13:40:52 +0900

    fix: 기존 router 컴포넌트 내용 이동
    Header 컴포넌트에서 Linkt를 사용하기 위해 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
-import Routers from "./Router";
-import { lightTheme } from "./utils/theme";
-
-const Wrapper = styled.div`
-  width: 390px;
-  margin: 0 auto;
-  background-color: ${({ theme }) => theme.bgColor};
-`;
+import Header from "./components/Header";
+import useDarkMode from "./hooks/useDarkMode";
+import About from "./pages/About";
+import Ask from "./pages/Ask";
+import Home from "./pages/Home";
+import Item from "./pages/Item";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
+import Show from "./pages/Show";
+import Top from "./pages/Top";
+import { GlobalStyle } from "./styles/globalStyles";
+import { darkTheme, lightTheme } from "./utils/theme";
 
 function App() {
+  const [themeMode, handleThemeMode] = useDarkMode();
+
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Wrapper>
-        <Routers />
-      </Wrapper>
+    <ThemeProvider theme={themeMode ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Header handleThemeMode={handleThemeMode} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/top" element={<Top />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/show" element={<Show />} />
+          <Route path="/ask" element={<Ask />} />
+          <Route path="/job" element={<Top />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/item/:id" element={<Item />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
