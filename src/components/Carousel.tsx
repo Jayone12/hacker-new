4c3547283019825d3cc7cf30ec600e62e7129cbe
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-16 17:33:19 +0900

    feat: Carousel 컴포넌트 생성

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,47 @@
+import ReactSlick, { Settings } from "react-slick";
+import styled from "styled-components";
+
+const StyledSlider = styled(ReactSlick)`
+  .slick-slide {
+    padding-bottom: 10px;
+  }
+  .slick-dots {
+    button {
+      &:before {
+        color: #b7b7b7;
+      }
+    }
+    .slick-active {
+      button {
+        &:before {
+          color: #ed702d;
+        }
+      }
+    }
+  }
+`;
+
+interface Props {
+  settings?: Settings;
+  children: JSX.Element[];
+}
+
+const DEFAUT_SETTINGS: Settings = {
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 500,
+  swipe: true,
+  draggable: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+const Carousel = ({ settings = DEFAUT_SETTINGS, children }: Props) => {
+  return (
+    <div>
+      <StyledSlider {...settings}>{children}</StyledSlider>
+    </div>
+  );
+};
+export default Carousel;
