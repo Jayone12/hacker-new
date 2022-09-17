2daf3ed88a13d785800543dbeff1551feb18eadf
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-17 15:39:32 +0900

    feat: Top stories 컨테이너 component 생성

---
+++ src/features/Home/Top/Stories.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import Story from "./Story";
+import { IStory } from "../../../types/types";
+
+const Container = styled.ul`
+  width: 350px !important;
+  margin-top: 20px;
+  margin-left: 20px;
+  padding: 0 14px;
+  box-sizing: border-box;
+  background-color: ${({ theme }) => theme.storyBg};
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1), 0px 6px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+`;
+
+interface Props {
+  stories?: (IStory | undefined)[];
+}
+
+const Stories = ({ stories }: Props) => {
+  return (
+    <Container>
+      {stories?.map((story, index) => (
+        <Story story={story} index={index} key={story?.id} />
+      ))}
+    </Container>
+  );
+};
+export default Stories;
