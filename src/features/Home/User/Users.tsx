3a69ef7f261bb449aae1c5df7addb62d0cff3204
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-19 23:36:03 +0900

    feat: Users container 컴포넌트 생성

---
+++ src/features/Home/User/Users.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import useBestDataFetch from "./useBestDataFetch";
+import User from "./User";
+
+const Container = styled.ul`
+  margin-top: 20px;
+  margin-bottom: 56px;
+  display: flex;
+  position: absolute;
+  padding-left: 20px;
+`;
+
+const Users = () => {
+  const { data: bestUsers, isLoading } = useBestDataFetch();
+
+  return (
+    <Container>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <>
+          {bestUsers?.map((user) => (
+            <User key={user?.id} {...user} />
+          ))}
+        </>
+      )}
+    </Container>
+  );
+};
+export default Users;
