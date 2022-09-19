2e3680e6fd4ec327db31bb8c437e3570f1fe8154
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-19 23:54:04 +0900

    feat: User 데이터 호출 로직 추가

--- src/apis.ts
+++ src/apis.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
-import { IStory } from "./types/types";
+import { IStory, IUser } from "./types/types";
 
 const BASE_URL = "https://hacker-news.firebaseio.com/v0/";
 
@@ -14,12 +14,46 @@ const getStory = async (id: number) => {
   }
 };
 
+const getUser = async (name: string) => {
+  try {
+    const { data: user }: AxiosResponse<IUser, AxiosError> = await axios.get(
+      `${BASE_URL}/user/${name}.json`
+    );
+    return user;
+  } catch (error) {
+    console.log("Error while getting a user.");
+  }
+};
+
 export const getTopStories = async () => {
   try {
     const { data: storyIds }: AxiosResponse<number[], AxiosError> =
       await axios.get(`${BASE_URL}/topstories.json`);
     const stories = await Promise.all(storyIds.slice(0, 15).map(getStory));
     return stories;
+  } catch {
+    console.log("Error while getting list of stories.");
+  }
+};
+
+export const getNewStories = async () => {
+  try {
+    const { data: storyIds }: AxiosResponse<number[], AxiosError> =
+      await axios.get(`${BASE_URL}/newstories.json`);
+    const stories = await Promise.all(storyIds.slice(0, 4).map(getStory));
+    return stories;
+  } catch {
+    console.log("Error while getting list of stories.");
+  }
+};
+
+export const getBestUsers = async () => {
+  try {
+    const { data: storyIds }: AxiosResponse<number[], AxiosError> =
+      await axios.get(`${BASE_URL}/beststories.json`);
+    const stories = await Promise.all(storyIds.slice(0, 5).map(getStory));
+    const users = await Promise.all(stories.map((story) => getUser(story!.by)));
+    return users;
   } catch {
     console.log("Error while getting list of stories.");
   }
