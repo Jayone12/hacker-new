0683f25f917ba4539816c618c63ad1a9c1ca64a3
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-19 23:29:39 +0900

    feat: Today's user 영역 컴포넌트 생성

---
+++ src/features/Home/User/index.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { ReactComponent as Arrow } from "../../../assets/images/title_arrow.svg";
+import BorderLine from "../../../components/BorderLine";
+import Stories from "./Stories";
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: 40px;
+  padding: 0 20px;
+`;
+
+const Title = styled.h3`
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: -0.004px;
+`;
+
+const StoriesContainer = styled.div`
+  position: relative;
+  height: 222px;
+  overflow-x: scroll;
+  padding-bottom: 10px;
+`;
+
+const TodayUsers = () => {
+  return (
+    <Container>
+      <TitleContainer>
+        <Title>
+          Today's User <Arrow />
+        </Title>
+      </TitleContainer>
+      <StoriesContainer>
+        <Stories />
+      </StoriesContainer>
+      <BorderLine />
+    </Container>
+  );
+};
+export default TodayUsers;
