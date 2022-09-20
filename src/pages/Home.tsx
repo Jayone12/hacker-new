81081731ac60871efe6ebeabd209aadc5e10aeb4
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-20 21:27:03 +0900

    feat: Home 페이지 완료

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,31 @@
+import styled from "styled-components";
+import banner from "../assets/images/home_banner.png";
+import AskStories from "../features/Home/Ask";
+import JobStories from "../features/Home/Job";
+import NewStories from "../features/Home/New";
+import ShowStories from "../features/Home/Show";
+import TopStories from "../features/Home/Top";
+import TodayUsers from "../features/Home/User";
+
+const Container = styled.section``;
+
+const Banner = styled.img`
+  width: 390px;
+  height: 240px;
+  margin-top: 2px;
+`;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Banner src={banner} alt="" />
+      <TopStories />
+      <NewStories />
+      <TodayUsers />
+      <ShowStories />
+      <AskStories />
+      <JobStories />
+    </Container>
+  );
 };
 export default Home;
