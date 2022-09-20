48b7608d91e8a987e874d07e853929803f1008db
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-20 21:17:49 +0900

    feat: Today's job 영역 컴포넌트 생성

---
+++ src/features/Home/Job/index.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { ReactComponent as Arrow } from "../../../assets/images/title_arrow.svg";
+import Stories from "./Stories";
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: 40px;
+  padding: 0 20px;
+`;
+
+const Title = styled.h3`
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: -0.004px;
+`;
+
+const StoriesContainer = styled.div`
+  position: relative;
+  height: 200px;
+  overflow-x: scroll;
+  padding-bottom: 10px;
+`;
+
+const JobStories = () => {
+  return (
+    <Container>
+      <TitleContainer>
+        <Title>
+          Today's Job <Arrow />
+        </Title>
+      </TitleContainer>
+      <StoriesContainer>
+        <Stories />
+      </StoriesContainer>
+    </Container>
+  );
+};
+export default JobStories;
