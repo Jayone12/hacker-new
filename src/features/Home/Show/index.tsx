67e9774a467e53e6b300411f3c73d6a7b741d2b6
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-20 10:26:37 +0900

    feat: Show stories 영역 컴포넌트 생성

---
+++ src/features/Home/Show/index.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { ReactComponent as Arrow } from "../../../assets/images/title_arrow.svg";
+import { ReactComponent as Update } from "../../../assets/images/update_icon.svg";
+import BorderLine from "../../../components/BorderLine";
+import Stories from "./Stories";
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: 40px;
+  padding: 0 20px;
+`;
+
+const Title = styled.h3`
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: -0.004px;
+`;
+
+const Refresh = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StoriesContainer = styled.div`
+  position: relative;
+  height: 222px;
+  overflow-x: scroll;
+  padding-bottom: 10px;
+  height: 300px;
+`;
+
+const ShowStories = () => {
+  return (
+    <Container>
+      <TitleContainer>
+        <Title>
+          Today's Show <Arrow />
+        </Title>
+        <Refresh>
+          {/* <button onClick={() => refetch()}> */}
+          <Update />
+          {/* </button> */}
+        </Refresh>
+      </TitleContainer>
+      <StoriesContainer>
+        <Stories />
+      </StoriesContainer>
+      <BorderLine />
+    </Container>
+  );
+};
+export default ShowStories;
