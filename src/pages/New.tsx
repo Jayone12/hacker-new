b6e8fdf965f4adc7059ab0186d2027995e536275
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-26 22:12:39 +0900

    feat: New page 생성

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,4 +1,69 @@
+import styled from "styled-components";
+import NewBanner from "../assets/images/new_banner.png";
+import LayoutButtons from "../components/LayoutButtons";
+import Pagination from "../components/Pagination";
+import Stories from "../components/Stories";
+import useStoryFetch from "../features/New/useStoryFetch";
+import useChangeLayout from "../hooks/useChangeLayout";
+import usePagination from "../hooks/usePagination";
+
+const Container = styled.section``;
+
+const ContentConatiner = styled.div`
+  padding: 20px;
+`;
+
+const Banner = styled.div`
+  margin-top: 2px;
+`;
+
+const Kinds = styled.div`
+  background-color: ${({ theme }) => theme.storyBg};
+  border-radius: 100px;
+  padding: 4px 5px;
+`;
+
+const Button = styled.button`
+  a {
+    display: block;
+    width: 170px;
+    padding: 12px 0;
+    font-size: 12px;
+    text-align: center;
+    border-radius: 100px;
+    &.active {
+      background-color: #ed702d;
+    }
+  }
+`;
+
 const New = () => {
-  return <div>New</div>;
+  const [layout, handlechangeLayout] = useChangeLayout();
+  const { limit, page, setPage, offset } = usePagination(1, 10);
+  const { data: stories, isLoading } = useStoryFetch(
+    offset,
+    offset + limit,
+    page
+  );
+
+  return (
+    <Container>
+      <Banner>
+        <img src={NewBanner} alt="NEW Articles" />
+      </Banner>
+      <ContentConatiner>
+        <LayoutButtons
+          layout={layout}
+          handlechangeLayout={handlechangeLayout}
+        />
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : (
+          <Stories layout={layout} stories={stories} />
+        )}
+        <Pagination page={page} setPage={setPage} />
+      </ContentConatiner>
+    </Container>
+  );
 };
 export default New;
