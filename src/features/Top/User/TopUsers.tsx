c9ad43f8b353afa4e6571b01ee490f2c21f67ae5
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-26 17:55:48 +0900

    feat: User 탭의 랭킹 리스트 컴포넌트 생성

---
+++ src/features/Top/User/TopUsers.tsx
@@ -0,0 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
+import styled from "styled-components";
+import { getTopUsers } from "../../../apis";
+import { IUser } from "../../../types/types";
+import UserList from "./UserList";
+
+const Container = styled.div`
+  padding-bottom: 50px;
+`;
+const ListTitles = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  background-color: #e2ecf0;
+  padding: 15px 0;
+  color: #727272;
+  font-size: 14px;
+  font-weight: 600;
+  margin-top: 20px;
+`;
+const ListTitle = styled.span`
+  width: 100%;
+`;
+
+const UserLists = styled.ul``;
+
+const TopUsers = () => {
+  const { data: users, isLoading } = useQuery(["topUser"], getTopUsers, {
+    select: (users) => {
+      return users?.sort((a, b) => b!.karma - a!.karma);
+    },
+  });
+
+  return (
+    <Container>
+      <ListTitles>
+        <ListTitle>Rank</ListTitle>
+        <ListTitle>User Name</ListTitle>
+        <ListTitle>Karma</ListTitle>
+      </ListTitles>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <UserLists>
+          {users?.map((user, index) => (
+            <UserList key={`${user?.id}${index}`} {...user} index={index} />
+          ))}
+        </UserLists>
+      )}
+    </Container>
+  );
+};
+export default TopUsers;
