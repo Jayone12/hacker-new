b03524817a5718ebfb20cd51a4fc5af82f19be6f
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-26 17:56:27 +0900

    feat: Top user 리스트 api 로직 추가

--- src/apis.ts
+++ src/apis.ts
@@ -17,7 +17,7 @@ const getStory = async (id: number) => {
 const getUser = async (name: string) => {
   try {
     const { data: user }: AxiosResponse<IUser, AxiosError> = await axios.get(
-      `${BASE_URL}/user/${name}.json`
+      `${BASE_URL}user/${name}.json`
     );
     return user;
   } catch (error) {
@@ -98,6 +98,18 @@ export const getTopStory = async (start: number, end: number) => {
       await axios.get(`${BASE_URL}/topstories.json`);
     const stories = await Promise.all(storyIds.slice(start, end).map(getStory));
     return stories;
+  } catch {
+    console.log("Error while getting list of stories.");
+  }
+};
+
+export const getTopUsers = async () => {
+  try {
+    const { data: storyIds }: AxiosResponse<number[], AxiosError> =
+      await axios.get(`${BASE_URL}/topstories.json`);
+    const stories = await Promise.all(storyIds.slice(0, 50).map(getStory));
+    const users = await Promise.all(stories.map((story) => getUser(story!.by)));
+    return users;
   } catch {
     console.log("Error while getting list of stories.");
   }
