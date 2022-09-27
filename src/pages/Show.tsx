6dbf18f946cedd491fc3a8e42deff835ea77e310
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-27 21:31:11 +0900

    feat: Show page 생성

--- src/pages/Show.tsx
+++ src/pages/Show.tsx
@@ -1,4 +1,52 @@
+import styled from "styled-components";
+import ShowBanner from "../assets/images/show_banner.png";
+import LayoutButtons from "../components/LayoutButtons";
+import Pagination from "../components/Pagination";
+import Stories from "../components/Stories";
+import useStoryFetch from "../features/Show/useStoryFetch";
+import useChangeLayout from "../hooks/useChangeLayout";
+import usePagination from "../hooks/usePagination";
+
+const Container = styled.section``;
+
+const ContentConatiner = styled.div`
+  padding: 20px;
+`;
+
+const Banner = styled.div`
+  margin-top: 2px;
+`;
+
 const Show = () => {
-  return <div>Show</div>;
+  const [layout, handlechangeLayout] = useChangeLayout();
+  const { limit, page, setPage, offset } = usePagination(1, 10);
+  const { data: stories, isLoading } = useStoryFetch(
+    offset,
+    offset + limit,
+    page
+  );
+
+  return (
+    <Container>
+      <Banner>
+        <img
+          src={ShowBanner}
+          alt="show your TALENTS, Share anything here, Experience hacker effect"
+        />
+      </Banner>
+      <ContentConatiner>
+        <LayoutButtons
+          layout={layout}
+          handlechangeLayout={handlechangeLayout}
+        />
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : (
+          <Stories layout={layout} stories={stories} />
+        )}
+        <Pagination page={page} setPage={setPage} />
+      </ContentConatiner>
+    </Container>
+  );
 };
 export default Show;
