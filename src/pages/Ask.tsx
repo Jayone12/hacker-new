7ed2300b6a6f9798653b9ad97671c708b1dd194c
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-27 21:43:39 +0900

    feat: Ask page 생성

--- src/pages/Ask.tsx
+++ src/pages/Ask.tsx
@@ -1,4 +1,38 @@
+import styled from "styled-components";
+import AskBanner from "../assets/images/ask_banner.png";
+import Pagination from "../components/Pagination";
+import useStoryFetch from "../features/Ask/useStoryFetch";
+import Stories from "../features/Ask/Stories";
+import useChangeLayout from "../hooks/useChangeLayout";
+import usePagination from "../hooks/usePagination";
+
+const Container = styled.section``;
+
+const ContentConatiner = styled.div``;
+
+const Banner = styled.div`
+  margin-top: 2px;
+`;
+
 const Ask = () => {
-  return <div>Ask</div>;
+  const [layout, handlechangeLayout] = useChangeLayout();
+  const { limit, page, setPage, offset } = usePagination(1, 10);
+  const { data: stories, isLoading } = useStoryFetch(
+    offset,
+    offset + limit,
+    page
+  );
+
+  return (
+    <Container>
+      <Banner>
+        <img src={AskBanner} alt="We're here!" />
+      </Banner>
+      <ContentConatiner>
+        {isLoading ? <span>Loading...</span> : <Stories stories={stories} />}
+        <Pagination page={page} setPage={setPage} />
+      </ContentConatiner>
+    </Container>
+  );
 };
 export default Ask;
