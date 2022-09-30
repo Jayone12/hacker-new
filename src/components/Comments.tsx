571619d7b691fee10c0f3d6ed761c459303c1176
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-30 18:51:35 +0900

    feat: sort 로직 추가
    sort 로직 추가 및 api 로직 수정

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,9 @@
 import { ReactComponent as CommentIcon } from "../assets/images/comment_icon.svg";
 import styled from "styled-components";
 import Comment from "./Comment";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getComments } from "../apis";
 
 const Container = styled.div`
   padding: 0 20px;
@@ -16,14 +19,15 @@ const CommentsSort = styled.div`
   display: flex;
 `;
 
-const SortBtn = styled.button`
+const SortBtn = styled.button<{ sort: boolean }>`
   display: flex;
   align-items: center;
   margin-right: 10px;
+  color: ${({ sort }) => !sort && "#727272"};
 `;
 
-const Circle = styled.div`
-  background-color: #f49664;
+const Circle = styled.div<{ sort: boolean }>`
+  background-color: ${({ sort }) => (sort ? "#f49664" : "#c4c4c4")};
   width: 6px;
   height: 6px;
   border-radius: 10px;
@@ -44,28 +48,47 @@ const CommentsValue = styled.div`
   }
 `;
 
-const Comments = ({ comments }: { comments?: number[] }) => {
+const Comments = ({ commentIds }: { commentIds?: number[] }) => {
+  const [sort, setSort] = useState(false);
+  const { data: comments, isLoading } = useQuery(["comments"], () =>
+    getComments(commentIds)
+  );
+
+  const arr = comments?.map((comment) => comment);
+
+  const sortComment = arr?.sort((a, b) => {
+    if (a!.time < b!.time) return 1;
+    if (a!.time > b!.time) return -1;
+    return 0;
+  });
+
+  const toggleHandler = () => setSort((prev) => !prev);
+
   return (
     <Container>
       <CommentsNav>
         <CommentsSort>
-          <SortBtn>
-            <Circle />
+          <SortBtn onClick={toggleHandler} disabled={!sort} sort={!sort}>
+            <Circle sort={!sort} />
             <Registered>Registered</Registered>
           </SortBtn>
-          <SortBtn>
-            <Circle />
+          <SortBtn onClick={toggleHandler} disabled={sort} sort={sort}>
+            <Circle sort={sort} />
             <Newest>Newest</Newest>
           </SortBtn>
         </CommentsSort>
         <CommentsValue>
           <CommentIcon width="12px" />
-          {comments ? comments.length : 0}
+          {commentIds ? commentIds.length : 0}
         </CommentsValue>
       </CommentsNav>
-      {comments?.map((comment) => (
-        <Comment key={comment} comment={comment} reComment={false} />
-      ))}
+      {sort
+        ? sortComment?.map((comment) => (
+            <Comment key={comment?.id} {...comment} reComment={false} />
+          ))
+        : comments?.map((comment) => (
+            <Comment key={comment?.id} {...comment} reComment={false} />
+          ))}
     </Container>
   );
 };
