ca1fc7c5837cde600e295bdedad8baafcfdd9b67
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-09-30 19:16:33 +0900

    refactor: 대댓글에 대한 api 로직 추가

--- src/components/ReComment.tsx
+++ src/components/ReComment.tsx
@@ -1,14 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { getComments } from "../apis";
 import Comment from "./Comment";
 
 interface Props {
-  comments: number[];
+  commentIds: number[];
+  parentId?: number;
 }
 
-const ReComment = ({ comments }: Props) => {
+const ReComment = ({ commentIds, parentId }: Props) => {
+  const { data: comments, isLoading } = useQuery(["reComments", parentId], () =>
+    getComments(commentIds)
+  );
+
   return (
     <>
-      {comments.map((comment) => (
-        <Comment comment={comment} key={comment} reComment={true} />
+      {comments?.map((comment) => (
+        <Comment key={comment?.id} reComment={true} {...comment} />
       ))}
     </>
   );
