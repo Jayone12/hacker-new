001323ccc79a8c1a0d4b035456b8680e4e729483
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-03 15:17:02 +0900

    feat: About 페이지 생성

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,22 @@
+import styled from "styled-components";
+import Content from "../features/About/Content";
+import TEMPLATES from "../features/About/Templates";
+
+const Container = styled.section`
+  padding: 20px;
+`;
+
+const ContentsContainer = styled.ul``;
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Container>
+      <ContentsContainer>
+        {TEMPLATES.map((content) => (
+          <Content key={content.title} {...content} />
+        ))}
+      </ContentsContainer>
+    </Container>
+  );
 };
 export default About;
