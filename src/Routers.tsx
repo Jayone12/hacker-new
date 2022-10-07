dcf31fdfde07bceb65c99268a91546a0cefbdcde
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-07 16:02:54 +0900

    refactor: Routers 분리 컴포넌트 생성

---
+++ src/Routers.tsx
@@ -0,0 +1,46 @@
+import { Route, Routes } from "react-router";
+import About from "./pages/About";
+import Ask from "./pages/Ask";
+import Home from "./pages/Home";
+import Item from "./pages/Item";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
+import Show from "./pages/Show";
+import Top from "./pages/Top";
+import TopPost from "./features/Top/Post/TopPost";
+import TopUsers from "./features/Top/User/TopUsers";
+import Job from "./pages/Job";
+import User from "./pages/User";
+import Submissions from "./features/User/Submissions";
+import Comments from "./features/User/Comments";
+import Favorites from "./features/User/Favorites";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/top/*" element={<Top />}>
+        <Route path="post" element={<TopPost />} />
+        <Route path="users" element={<TopUsers />} />
+      </Route>
+      <Route path="/top/item/:id" element={<Item />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/new/item/:id" element={<Item />} />
+      <Route path="/show" element={<Show />} />
+      <Route path="/show/item/:id" element={<Item />} />
+      <Route path="/ask" element={<Ask />} />
+      <Route path="/ask/item/:id" element={<Item />} />
+      <Route path="/job" element={<Job />} />
+      <Route path="/job/item/:id" element={<Item />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/user/item/:id" element={<Item />} />
+      <Route path="/user/:name/*" element={<User />}>
+        <Route path="submissions" element={<Submissions />} />
+        <Route path="comments" element={<Comments />} />
+        <Route path="favorites" element={<Favorites />} />
+      </Route>
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  );
+};
+export default Routers;
