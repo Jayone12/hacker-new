7ba2e0c92e7a2a3e2562dc13fef3edff3a12a007
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-08 09:04:24 +0900

    feat: 프로젝트 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styled, { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import useDarkMode from "./hooks/useDarkMode";
-import About from "./pages/About";
-import Ask from "./pages/Ask";
-import Home from "./pages/Home";
-import Item from "./pages/Item";
-import New from "./pages/New";
-import NotFound from "./pages/NotFound";
-import Show from "./pages/Show";
-import Top from "./pages/Top";
+import Routers from "./Routers";
+
 import { GlobalStyle } from "./styles/globalStyles";
 import { darkTheme, lightTheme } from "./utils/theme";
 
@@ -21,17 +15,7 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Header handleThemeMode={handleThemeMode} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/top" element={<Top />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/show" element={<Show />} />
-          <Route path="/ask" element={<Ask />} />
-          <Route path="/job" element={<Top />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/item/:id" element={<Item />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
+        <Routers />
       </BrowserRouter>
     </ThemeProvider>
   );
