e9aa3dfebab82cdebc12818deefe676b4bb3471a
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-09 12:49:02 +0900

    refactor: Banner 컴포넌트로 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,23 @@
-import styled from "styled-components";
-import banner from "../assets/images/home_banner.png";
+import Banner from "../components/Banner";
 import AskStories from "../features/Home/Ask";
 import JobStories from "../features/Home/Job";
 import NewStories from "../features/Home/New";
 import ShowStories from "../features/Home/Show";
 import TopStories from "../features/Home/Top";
 import TodayUsers from "../features/Home/User";
-
-const Container = styled.section``;
-
-const Banner = styled.img`
-  width: 390px;
-  height: 240px;
-  margin-top: 2px;
-`;
+import banner from "../assets/images/home_banner.png";
 
 const Home = () => {
   return (
-    <Container>
-      <Banner src={banner} alt="" />
+    <section>
+      <Banner src={banner} alt="Use hacker News! For your Hacker Effect!" />
       <TopStories />
       <NewStories />
       <TodayUsers />
       <ShowStories />
       <AskStories />
       <JobStories />
-    </Container>
+    </section>
   );
 };
 export default Home;
