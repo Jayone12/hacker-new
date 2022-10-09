88aa31c471d7b76fcde69ec75a2cfde1b8dc8bb5
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-09 15:57:40 +0900

    불필요한 코드 제거

--- src/features/Home/Job/Stories.tsx
+++ src/features/Home/Job/Stories.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 import Story from "./Story";
-import { useQuery } from "@tanstack/react-query";
-import { getStories } from "../../../apis";
 import { IStory } from "../../../types/types";
 
 const Container = styled.ul`
