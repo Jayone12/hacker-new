f7672ec92dcbf10615a63e2925e97420fbd3f66b
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-10 14:02:13 +0900

    refactor: 비즈니스 로직과 view 분리

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,45 +1,19 @@
-import styled from "styled-components";
-import NewBanner from "../assets/images/new_banner.png";
-import LayoutButtons from "../components/LayoutButtons";
-import Pagination from "../components/Pagination";
-import Stories from "../components/Stories";
-
+import NewContent from "../features/New";
 import useChangeLayout from "../hooks/useChangeLayout";
-import usePagination from "../hooks/usePagination";
 import useStoryFetch from "../hooks/useStoryFetch";
-
-const Container = styled.section``;
-
-const ContentConatiner = styled.div`
-  padding: 20px;
-`;
-
-const Banner = styled.div`
-  margin-top: 2px;
-`;
 
 const New = () => {
   const [layout, handlechangeLayout] = useChangeLayout();
   const { data: stories, isLoading, page, setPage } = useStoryFetch("new");
+  const props = {
+    layout,
+    handlechangeLayout,
+    stories,
+    isLoading,
+    page,
+    setPage,
+  };
 
-  return (
-    <Container>
-      <Banner>
-        <img src={NewBanner} alt="NEW Articles" />
-      </Banner>
-      <ContentConatiner>
-        <LayoutButtons
-          layout={layout}
-          handlechangeLayout={handlechangeLayout}
-        />
-        {isLoading ? (
-          <span>Loading...</span>
-        ) : (
-          <Stories layout={layout} stories={stories} />
-        )}
-        <Pagination page={page} setPage={setPage} />
-      </ContentConatiner>
-    </Container>
-  );
+  return <NewContent {...props} />;
 };
 export default New;
