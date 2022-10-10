e589eaafe3b659fbb753cfef988b476065233a22
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-10 14:38:08 +0900

    refactor: 비즈니스 로직과 view 분리

---
+++ src/features/User/index.tsx
@@ -0,0 +1,137 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import Comments from "../../components/Comments";
+import { IUser } from "../../types/types";
+import { diffTime } from "../../utils/diffTime";
+import Favorites from "./Favorites";
+import Submissions from "./Submissions";
+
+const Container = styled.section`
+  margin-top: 2px;
+  padding: 20px;
+`;
+
+const UserContents = styled.div`
+  background-color: ${({ theme }) => theme.storyBg};
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1), 0px 3px 6px rgba(0, 0, 0, 0.05);
+  border-radius: 8px;
+  padding: 0 16px;
+`;
+
+const UserName = styled.h2`
+  font-size: 22px;
+  font-weight: 700;
+  padding: 24px 0 12px;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  padding: 10px 0;
+`;
+
+const Joined = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  color: ${({ theme }) => theme.fontColor};
+`;
+
+const Karma = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  color: ${({ theme }) => theme.fontColor};
+  margin-left: 15px;
+`;
+
+const Tag = styled.span`
+  color: #ed702d;
+  font-size: 10px;
+  padding: 2px 5px;
+  border: 1px solid #ed702d;
+  border-radius: 20px;
+  margin-right: 5px;
+`;
+
+const Introduce = styled.p`
+  padding: 24px 0;
+`;
+
+const Taps = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid ${({ theme }) => theme.borderColor};
+  border-radius: 1000px;
+  padding: 5px;
+  margin-top: 32px;
+  margin-bottom: 20px;
+`;
+
+const Tap = styled.li`
+  flex: 1;
+  > a {
+    display: block;
+    border-radius: 100px;
+    padding: 10px 0;
+    text-align: center;
+    font-size: 12px;
+    &.active {
+      background-color: #ed702d;
+    }
+  }
+`;
+
+interface Props {
+  user?: IUser;
+  isLoaing?: boolean;
+}
+
+const UserContainer = ({ user, isLoaing }: Props) => {
+  return (
+    <Container>
+      <UserContents>
+        <UserName>{user?.id}</UserName>
+        <UserInfo>
+          <Joined>
+            <Tag>Joined</Tag>
+            {diffTime(user?.created!)}
+          </Joined>
+          <Karma>
+            <Tag>Karma</Tag>
+            {user?.karma}
+          </Karma>
+        </UserInfo>
+        {user?.about && (
+          <Introduce
+            dangerouslySetInnerHTML={{
+              __html: `${user?.about}`,
+            }}
+          />
+        )}
+      </UserContents>
+      <Taps>
+        <Tap>
+          <NavLink to="submissions">Submissions</NavLink>
+        </Tap>
+        <Tap>
+          <NavLink to="comments">Comments</NavLink>
+        </Tap>
+        <Tap>
+          <NavLink to="favorites">Favorites</NavLink>
+        </Tap>
+      </Taps>
+      <Routes>
+        <Route
+          path="submissions"
+          element={<Submissions ids={user?.submitted} />}
+        />
+        <Route
+          path="comments"
+          element={<Comments commentIds={user?.submitted} />}
+        />
+        <Route path="favorites" element={<Favorites />} />
+      </Routes>
+    </Container>
+  );
+};
+export default UserContainer;
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,137 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
-import { Route, Routes, useParams } from "react-router";
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
-import { idText } from "typescript";
+import { useParams } from "react-router";
 import { getUser } from "../apis";
-import Comments from "../features/User/Comments";
-import Favorites from "../features/User/Favorites";
-import Submissions from "../features/User/Submissions";
-import { diffTime } from "../utils/diffTime";
-
-const Container = styled.section`
-  margin-top: 2px;
-  padding: 20px;
-`;
-
-const UserContents = styled.div`
-  background-color: ${({ theme }) => theme.storyBg};
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1), 0px 3px 6px rgba(0, 0, 0, 0.05);
-  border-radius: 8px;
-  padding: 0 16px;
-`;
-
-const UserName = styled.h2`
-  font-size: 22px;
-  font-weight: 700;
-  padding: 24px 0 12px;
-`;
-
-const UserInfo = styled.div`
-  display: flex;
-  padding: 10px 0;
-`;
-
-const Joined = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 12px;
-  color: ${({ theme }) => theme.fontColor};
-`;
-
-const Karma = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 12px;
-  color: ${({ theme }) => theme.fontColor};
-  margin-left: 15px;
-`;
-
-const Tag = styled.span`
-  color: #ed702d;
-  font-size: 10px;
-  padding: 2px 5px;
-  border: 1px solid #ed702d;
-  border-radius: 20px;
-  margin-right: 5px;
-`;
-
-const Introduce = styled.p`
-  padding: 24px 0;
-`;
-
-const Taps = styled.ul`
-  display: flex;
-  justify-content: space-between;
-  border: 1px solid ${({ theme }) => theme.borderColor};
-  border-radius: 1000px;
-  padding: 5px;
-  margin-top: 32px;
-  margin-bottom: 20px;
-`;
-
-const Tap = styled.li`
-  flex: 1;
-  > a {
-    display: block;
-    border-radius: 100px;
-    padding: 10px 0;
-    text-align: center;
-    font-size: 12px;
-    &.active {
-      background-color: #ed702d;
-    }
-  }
-`;
+import UserContainer from "../features/User";
 
 const User = () => {
   const { name } = useParams();
+  const { data: user, isLoading } = useQuery(["user", name], () =>
+    getUser(name!)
+  );
 
-  const { data, isLoading } = useQuery(["user", name], () => getUser(name!));
+  const props = {
+    user,
+    isLoading,
+  };
+
   if (!isLoading) {
-    return (
-      <Container>
-        <UserContents>
-          <UserName>{data?.id}</UserName>
-          <UserInfo>
-            <Joined>
-              <Tag>Joined</Tag>
-              {diffTime(data?.created!)}
-            </Joined>
-            <Karma>
-              <Tag>Karma</Tag>
-              {data?.karma}
-            </Karma>
-          </UserInfo>
-          {data?.about && (
-            <Introduce
-              dangerouslySetInnerHTML={{
-                __html: `${data?.about}`,
-              }}
-            />
-          )}
-        </UserContents>
-        <Taps>
-          <Tap>
-            <NavLink to="submissions">Submissions</NavLink>
-          </Tap>
-          <Tap>
-            <NavLink to="comments">Comments</NavLink>
-          </Tap>
-          <Tap>
-            <NavLink to="favorites">Favorites</NavLink>
-          </Tap>
-        </Taps>
-        <Routes>
-          <Route
-            path="submissions"
-            element={<Submissions ids={data?.submitted} />}
-          />
-          <Route path="comments" element={<Comments ids={data?.submitted} />} />
-          <Route path="favorites" element={<Favorites />} />
-        </Routes>
-      </Container>
-    );
+    return <UserContainer {...props} />;
   } else {
     return <span>Loading...</span>;
   }
