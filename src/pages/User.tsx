1bde19909aef2988dd9972ccb06c960e612af5bd
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-12 22:01:39 +0900

    feat: User submitted 데이터 호출 후 type 분할 로직 추가

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,17 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
-import { getUser } from "../apis";
+import { getSubmissions, getUser } from "../apis";
 import UserContainer from "../features/User";
 
 const User = () => {
   const { name } = useParams();
-  const { data: user, isLoading } = useQuery(["user", name], () =>
-    getUser(name!)
+  const { data: user } = useQuery(["user", name], () => getUser(name!));
+  const { data: submitted, isLoading } = useQuery(
+    ["submitted", name],
+    () => getSubmissions(user!.submitted),
+    {
+      enabled: !!user?.submitted,
+      select: (datas) => {
+        const stories = datas?.filter(
+          (data) => data?.type !== "comment" && !data?.deleted
+        );
+        const comments = datas?.filter(
+          (data) => data?.type === "comment" && !data?.deleted
+        );
+        return { stories, comments };
+      },
+    }
   );
 
   const props = {
     user,
     isLoading,
+    submitted,
   };
 
   if (!isLoading) {
