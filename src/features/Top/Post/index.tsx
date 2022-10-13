cd550f03b922145a3a32c56d87e49e9fdcbbeb80
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-10-13 12:35:29 +0900

    fix: Story의 전체 호출 및 total 페이지 숫자 추가

--- src/features/Top/Post/index.tsx
+++ src/features/Top/Post/index.tsx
@@ -1,23 +1,14 @@
-import styled from "styled-components";
 import useStoryFetch from "../../../hooks/useStoryFetch";
-import Pagination from "../../../components/Pagination";
 import useChangeLayout from "../../../hooks/useChangeLayout";
-import LayoutButtons from "../../../components/LayoutButtons";
-import Stories from "../../../components/Stories";
 import TopPostContent from "./TopPostContent";
-
-const Container = styled.div`
-  margin-top: 20px;
-  padding: 0 20px;
-`;
 
 const TopPost = () => {
   const [layout, handlechangeLayout] = useChangeLayout();
-  const { data: stories, isLoading, page, setPage } = useStoryFetch("top");
+  const { data, isLoading, page, setPage } = useStoryFetch("top");
   const props = {
     layout,
     handlechangeLayout,
-    stories,
+    data,
     isLoading,
     page,
     setPage,
